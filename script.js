831ca666ec72ec2b528659f4a1a43ab363f452e5
miloszcwen <cwen.milosz@gmail.com>
2021-01-15 13:55:28 +0100

    add array to store saved bg, add modal to show stored bg

--- script.js
+++ script.js
@@ -1,13 +1,10 @@
-import {without} from 'node_modules/lodash';
+const color1 = document.querySelector(".color1");
+const color2 = document.querySelector(".color2"); //class
+const text = document.querySelector("#text"); //id
+const body = document.querySelector("body"); //tag
+const random = document.querySelector(".random");
+const gradientValues = [];
 
-var color1 = document.querySelector(".color1");
-var color2 = document.querySelector(".color2"); //class
-var text = document.querySelector("#text"); //id
-var body = document.querySelector("body"); //tag
-var random = document.querySelector(".random");
-
-let arr =[1,2,3,4,5,6,7,8];
-console.log(without(arr,3))
 
 color1.addEventListener("input", setBgGradient);
 color2.addEventListener("input", setBgGradient);
@@ -18,13 +15,12 @@ setBgGradient();
 function setBgGradient (){
 	body.style.background = "linear-gradient(to right bottom, "
 	+color1.value+", "+color2.value+")";
-	writeGradient();
+	writeGradientOnScreen();
+	storeGradientValues();
 }
 
-function writeGradient (){
-	// text.append("background: linear-gradient: (to right, "
-	// 	+color1.value+", "+color2.value+");");
-	text.textContent = body.style.background + ";";
+function writeGradientOnScreen (){
+		text.textContent = body.style.background + ";";
 }
 
 function generateRandomBg (){
@@ -32,3 +28,37 @@ function generateRandomBg (){
 	color2.value = "#" + Math.floor(Math.random()*16777216).toString(16);
 	setBgGradient();
 }
+
+function storeGradientValues(){
+	gradientValues.push({c1: color1.value, c2: color2.value});
+	console.log(gradientValues);
+}
+
+//////////////////////////////////////////////////////////////////////////
+
+const modal = document.querySelector("#modal")
+const openModalButton = document.querySelector("#open-modal-btn")
+const closeModalButton = document.querySelector("#close-modal-btn")
+const overlay = document.querySelector("#overlay")
+const modalText = document.querySelector("#modal-text")
+
+openModalButton.addEventListener("click", () => {
+  modal.classList.add("open")
+  overlay.classList.add("open")
+  gradientValues.forEach((val)=>{
+	console.log(val)
+	modalText.textContent+=JSON.stringify(val);
+  })
+
+})
+
+closeModalButton.addEventListener("click", closeModal)
+
+overlay.addEventListener("click", closeModal)
+
+function closeModal() {
+  modal.classList.remove("open")
+  overlay.classList.remove("open")
+}
+
+//////////////////////////////////////////////////////////////////////////////
